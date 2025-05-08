2b8576b2d641ee663f0f596cff19dcc1a7852bc2
RamaKrishnaGujja1 <ramakrishnagujja744@gmail.com>
2025-05-08 11:11:03 -0500

    Config frontend update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: "frontend",
   plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, "frontend/dist"),
-    emptyOutDir: true,
-  },
 });
